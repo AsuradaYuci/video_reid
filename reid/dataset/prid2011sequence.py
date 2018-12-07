1f7f0c4818028545b989451a772e14185d6a44bc
Chenyang Yu <asuradayuci@gmail.com>
2018-12-07 21:10:07 +0800

    Add files via upload
    make code runable on the prid2011 dataset.

--- reid/dataset/prid2011sequence.py
+++ reid/dataset/prid2011sequence.py
@@ -5,13 +5,17 @@ from reid.data.datasequence import Datasequence
 from utils.osutils import mkdir_if_missing
 from utils.serialization import write_json
 import tarfile
-import zipfile
 from glob import glob
 import shutil
 import numpy as np
 
 datasetname = 'prid_2011'
 flowname = 'prid2011flow'
+
+
+class infostruct(object):
+    pass
+
 
 class PRID2011SEQUENCE(Datasequence):
 
@@ -26,6 +30,21 @@ class PRID2011SEQUENCE(Datasequence):
 
         self.load(seq_len, seq_srd, num_val)
 
+        self.query, query_pid, query_camid, query_num = self._pluckseq_cam(self.identities, self.split['query'],
+                                                                           seq_len, seq_srd, 0)
+        self.queryinfo = infostruct()
+        self.queryinfo.pid = query_pid
+        self.queryinfo.camid = query_camid
+        self.queryinfo.tranum = query_num
+
+        self.gallery, gallery_pid, gallery_camid, gallery_num = self._pluckseq_cam(self.identities,
+                                                                                   self.split['gallery'],
+                                                                                   seq_len, seq_srd, 1)
+        self.galleryinfo = infostruct()
+        self.galleryinfo.pid = gallery_pid
+        self.galleryinfo.camid = gallery_camid
+        self.galleryinfo.tranum = gallery_num
+
     @property
     def other_dir(self):
         return osp.join(self.root, 'others')
@@ -39,7 +58,7 @@ class PRID2011SEQUENCE(Datasequence):
         raw_dir = osp.join(self.root, 'raw')
         mkdir_if_missing(raw_dir)
 
-        fpath1 = osp.join(raw_dir, datasetname + '.zip')
+        fpath1 = osp.join(raw_dir, datasetname + '.tar')
         fpath2 = osp.join(raw_dir, flowname + '.tar')
 
         if osp.isfile(fpath1) and osp.isfile(fpath2):
@@ -51,18 +70,24 @@ class PRID2011SEQUENCE(Datasequence):
     def imgextract(self):
 
         raw_dir = osp.join(self.root, 'raw')
+        # raw_dir = /media/ying/0BDD17830BDD1783/video_reid _prid/data/prid2011sequence/raw
         exdir1 = osp.join(raw_dir, datasetname)
+        # exdir1 = /media/ying/0BDD17830BDD1783/video_reid _prid/data/prid2011sequence/raw/prid_2011
         exdir2 = osp.join(raw_dir, flowname)
-        fpath1 = osp.join(raw_dir, datasetname + '.zip')
+        # exdir2 = /media/ying/0BDD17830BDD1783/video_reid _prid/data/prid2011sequence/raw/prid2011flow
+        fpath1 = osp.join(raw_dir, datasetname + '.tar')
+        # fpath1 = /media/ying/0BDD17830BDD1783/video_reid _prid/data/prid2011sequence/raw/prid_2011.tar
         fpath2 = osp.join(raw_dir, flowname + '.tar')
+        # fpath2 = /media/ying/0BDD17830BDD1783/video_reid _prid/data/prid2011sequence/raw/prid2011flow.tar
 
         if not osp.isdir(exdir1):
             print("Extracting tar file")
             cwd = os.getcwd()
-            zip_ref = zipfile.ZipFile(fpath1, 'r')
+            tar_ref = tarfile.open(fpath1)
             mkdir_if_missing(exdir1)
-            zip_ref.extractall(exdir1)
-            zip_ref.close()
+            os.chdir(exdir1)
+            tar_ref.extractall()
+            tar_ref.close()
             os.chdir(cwd)
 
         if not osp.isdir(exdir2):
@@ -75,46 +100,87 @@ class PRID2011SEQUENCE(Datasequence):
             tar_ref.close()
             os.chdir(cwd)
 
-        ## recognizing the dataset
+        # recognizing the dataset
         # Format
+        temp_images_dir = osp.join(self.root, 'temp_images')
+        mkdir_if_missing(temp_images_dir)
+
+        temp_others_dir = osp.join(self.root, 'temp_others')
+        mkdir_if_missing(temp_others_dir)
 
         images_dir = osp.join(self.root, 'images')
         mkdir_if_missing(images_dir)
+        # images_dir = /media/ying/0BDD17830BDD1783/video_reid _prid/data/prid2011sequence/images
 
         others_dir = osp.join(self.root, 'others')
         mkdir_if_missing(others_dir)
+        # others_dir = /media/ying/0BDD17830BDD1783/video_reid _prid/data/prid2011sequence/others
 
-        fpaths1 = sorted(glob(osp.join(exdir1, 'multi_shot', '*/*/*.png')))
-        fpaths2 = sorted(glob(osp.join(exdir2, '*/*.png')))
+        fpaths1 = sorted(glob(osp.join(exdir1, 'prid_2011/multi_shot', '*/*/*.png')))
+        fpaths2 = sorted(glob(osp.join(exdir2, 'prid2011flow', '*/*/*.png')))
 
-        identities_images = [[[] for _ in range(2)] for _ in range(200)]
+        identities_imgraw = [[[] for _ in range(2)] for _ in range(200)]
+        identities_otherraw = [[[] for _ in range(2)] for _ in range(200)]
 
         for fpath in fpaths1:
             fname = fpath
             fname_list = fname.split('/')
-            cam_name = fname_list[-3]
-            pid_name = fname_list[-2]
-            frame_name = fname_list[-1]
-            cam_id = 1 if cam_name =='cam_a' else 2
-            pid_id = int(pid_name.split('_')[-1])
+            cam_name = fname_list[-3]  # cam_a  / cam_b
+            pid_name = fname_list[-2]  # person_001
+            frame_name = fname_list[-1]  # 0001.png
+            cam_id = 1 if cam_name == 'cam_a' else 2  # cam_id = 1 / 2
+            pid_id = int(pid_name.split('_')[-1])  # pid_id = 001
             if pid_id > 200:
                 continue
-            frame_id = int(frame_name.split('.')[-2])
-            imagefname = ('{:08d}_{:02d}_{:04d}.png'
+            frame_id = int(frame_name.split('.')[-2])  # frame_id = 0001
+            temp_fname = ('{:08d}_{:02d}_{:04d}.png'
                           .format(pid_id-1, cam_id-1, frame_id-1))
-            identities_images[pid_id - 1][cam_id - 1].append(imagefname)
-            shutil.copy(fpath, osp.join(images_dir, imagefname))
+            identities_imgraw[pid_id - 1][cam_id - 1].append(temp_fname)
+            shutil.copy(fpath, osp.join(temp_images_dir, temp_fname))
+
+        identities_temp = [x for x in identities_imgraw if x != [[], []]]
+        identities_images = identities_temp
+
+        for pid in range(len(identities_temp)):
+            for cam in range(2):
+                for img in range(len(identities_images[pid][cam])):
+                    temp_fname = identities_temp[pid][cam][img]
+                    fname = ('{:08d}_{:02d}_{:04d}.png'.format(pid, cam, img))
+                    identities_images[pid][cam][img] = fname
+                    shutil.copy(osp.join(temp_images_dir, temp_fname), osp.join(images_dir, fname))
+
+        shutil.rmtree(temp_images_dir)
 
         for fpath in fpaths2:
             fname = fpath
             fname_list = fname.split('/')
-            fname_img = fname_list[-1]
-            shutil.copy(fname, osp.join(others_dir, fname_img))
+            cam_name = fname_list[-3]  # cam_a  / cam_b
+            pid_name = fname_list[-2]  # person_001
+            frame_name = fname_list[-1]  # 0001.png
+            cam_id = 1 if cam_name == 'cam_a' else 2  # cam_id = 1 / 2
+            pid_id = int(pid_name.split('_')[-1])  # pid_id = 001
+            if pid_id > 200:
+                continue
+            frame_id = int(frame_name.split('.')[-2])  # frame_id = 0001
+            temp_fname = ('{:08d}_{:02d}_{:04d}.png'
+                          .format(pid_id-1, cam_id-1, frame_id-1))
+            identities_otherraw[pid_id - 1][cam_id - 1].append(temp_fname)
+            shutil.copy(fpath, osp.join(temp_others_dir, temp_fname))
 
+        identities_temp = [x for x in identities_otherraw if x != [[], []]]
+        identities_others = identities_temp
 
+        for pid in range(len(identities_temp)):
+            for cam in range(2):
+                for img in range(len(identities_others[pid][cam])):
+                    temp_fname = identities_temp[pid][cam][img]
+                    fname = ('{:08d}_{:02d}_{:04d}.png'.format(pid, cam, img))
+                    identities_images[pid][cam][img] = fname
+                    shutil.copy(osp.join(temp_others_dir, temp_fname), osp.join(others_dir, fname))
 
+        shutil.rmtree(temp_others_dir)
 
-        meta = {'name': 'iLIDS-sequence', 'shot': 'sequence', 'num_cameras': 2,
+        meta = {'name': 'prid-sequence', 'shot': 'sequence', 'num_cameras': 2,
                 'identities': identities_images}
 
         write_json(meta, osp.join(self.root, 'meta.json'))
@@ -123,7 +189,7 @@ class PRID2011SEQUENCE(Datasequence):
         splits = []
         for i in range(10):
             pids = np.random.permutation(num)
-            pids = (pids -1).tolist()
+            pids = (pids - 1).tolist()
             trainval_pids = pids[:num // 2]
             test_pids = pids[num // 2:]
             split = {'trainval': trainval_pids,
@@ -132,3 +198,23 @@ class PRID2011SEQUENCE(Datasequence):
 
             splits.append(split)
         write_json(splits, osp.join(self.root, 'splits.json'))
+
+    def _pluckseq_cam(self, identities, indices, seq_len, seq_str, camid):
+        ret = []
+        per_id = []
+        cam_id = []
+        tra_num = []
+
+        for index, pid in enumerate(indices):
+            pid_images = identities[pid]
+            cam_images = pid_images[camid]
+            seqall = len(cam_images)
+            seq_inds = [(start_ind, start_ind + seq_len) for start_ind in range(0, seqall - seq_len, seq_str)]
+            if not seq_inds:
+                seq_inds = [(0, seqall)]
+            for seq_ind in seq_inds:
+                ret.append((seq_ind[0], seq_ind[1], pid, index, camid))
+            per_id.append(pid)
+            cam_id.append(camid)
+            tra_num.append(len(seq_inds))
+        return ret, per_id, cam_id, tra_num
