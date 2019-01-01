b96bc861be2399bd21fd32cc748607bdc0d38dc3
Chenyang Yu <asuradayuci@gmail.com>
2019-01-01 20:41:45 +0800

    Add files via upload

--- reid/models/selfpoolingdir.py
+++ reid/models/selfpoolingdir.py
@@ -14,6 +14,8 @@ class SelfPoolingDir(nn.Module):
         # todo: LSTM
         self.lstm = nn.LSTM(input_size=self.input_num,
                             hidden_size=self.output_num, num_layers=1, batch_first=True, dropout=0)
+        self.bilstm = nn.LSTM(input_size=self.input_num, hidden_size=self.output_num,
+                              num_layers=1, batch_first=True, dropout=0, bidirectional=True)
         self.lstm_bn = nn.BatchNorm1d(self.output_num)
 
         ## Linear K
@@ -38,49 +40,41 @@ class SelfPoolingDir(nn.Module):
         init.constant_(self.featQ.bias, 0)
         init.constant_(self.featQ_bn.weight, 1)
         init.constant_(self.featQ_bn.bias, 0)
+        init.constant_(self.lstm_bn.weight, 1)
+        init.constant_(self.lstm_bn.bias, 0)
 
         # add lstm to generate q
     def forward(self, probe_value, probe_base):
-        pro_size = probe_value.size()
+        pro_size = probe_value.size()  # torch.Size([6, 8, 2048])
         pro_batch = pro_size[0]
         pro_len = pro_size[1]
 
-        # generating Querys
-        # Qs = probe_base.view(pro_batch * pro_len, -1)
-        # Qs = self.featQ(Qs)
-        # Qs = self.featQ_bn(Qs)
-        # Qs = Qs.view(pro_batch, pro_len, -1)
-        #
-        # Qmean = torch.mean(Qs, 1)
-        # Qs = Qmean.squeeze(1)
-        # Hs = Qmean.unsqueeze(1).expand(pro_batch, pro_len, self.output_num)
-
         # use LSTM generating Querys
-        Qs = probe_base.view(pro_batch, pro_len, -1)
-        Qs, (h_n, c_n) = self.lstm(Qs)  # QS = 4*8*128  h_n = 1*4*128 = Qs[,:-1,] c_n = 1*4*128
-        Qs = h_n.squeeze(1)  # Qs = h_n = 1*4*128
-        # Qs = Qs.permute(0, 2, 1)  # 4*128*10
-        # Qs = F.avg_pool1d(Qs, pro_len)  # 4*128*1
-        Qs = Qs.view(pro_batch, self.output_num)  # 4*128
+        Qs = probe_base.view(pro_batch, pro_len, -1)  # 6 * 8 * 2048
+        Qs, (h_n, c_n) = self.bilstm(Qs)  # QS = 6*8*128  h_n = 2*6*128 = Qs[,:-1,] c_n = 2*6*128
+        Qs = h_n[0, :, :] + h_n[1, :, :]  # Qs = h_n = 6*128
+        # Qs, (h_n, c_n) = self.lstm(Qs)
+        # Qs = h_n.squeeze(1)
+        Qs = Qs.view(pro_batch, self.output_num)  # 6*128
         Qs = self.lstm_bn(Qs)
 
-        Hs = Qs.unsqueeze(1).expand(pro_batch, pro_len, self.output_num)  # Hs = 4*8*128
+        Hs = Qs.unsqueeze(1).expand(pro_batch, pro_len, self.output_num)  # Hs = 6*8*128
         # generating Keys
-        K = probe_base.view(pro_batch * pro_len, -1)
+        K = probe_base.view(pro_batch * pro_len, -1)  # torch.Size([48, 2048])
         K = self.featK(K)
         K = self.featK_bn(K)
-        K = K.view(pro_batch, pro_len, -1)  # 4*8*128
+        K = K.view(pro_batch, pro_len, -1)  # 6*8*128
 
-        weights = Hs * K
-        weights = weights.permute(0, 2, 1)
+        weights = Hs * K  # torch.Size([6, 8, 128])
+        weights = weights.permute(0, 2, 1)  # torch.Size([6, 128, 8])
         weights = weights.contiguous()
-        weights = weights.view(-1, pro_len)
+        weights = weights.view(-1, pro_len)  # torch.Size([768, 8])
         weights = self.softmax(weights)
-        weights = weights.view(pro_batch, self.output_num, pro_len)
-        weights = weights.permute(0, 2, 1)
+        weights = weights.view(pro_batch, self.output_num, pro_len)  # torch.Size([6, 128, 8])
+        weights = weights.permute(0, 2, 1)  # torch.Size([6, 8, 128])
 
-        pool_probe = probe_value * weights
-        pool_probe = pool_probe.sum(1)
+        pool_probe = probe_value * weights  # torch.Size([6, 8, 128])
+        pool_probe = pool_probe.sum(1)  # torch.Size([6, 128])
         pool_probe = pool_probe.squeeze(1)
 
         # pool_probe = torch.mean(probe_value, 1)
